613b0a6154dd98a251775691a31e413072e66aba
Damir Nabiullin <nabiull2020@gmail.com>
2023-10-12 20:22:32 +0300

    Added settings router

--- quranapp_backend/src/main.py
+++ quranapp_backend/src/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from fastapi import FastAPI
-from src.controllers import recordings, users
+from src.controllers import recordings, users, settings
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -11,3 +11,4 @@ app = FastAPI()
 # Include routers in the app
 app.include_router(users)
 app.include_router(recordings)
+app.include_router(settings)
