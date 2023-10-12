737bfe13ed7dceca1d82d9aa7eac15f0c1c8ce78
Damir Nabiullin <nabiull2020@gmail.com>
2023-10-12 20:51:19 +0300

    DB works with env variables

--- quranapp_backend/src/DAL/database.py
+++ quranapp_backend/src/DAL/database.py
@@ -1,9 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+USER = os.getenv('DB_USER')
+PASSWORD = os.environ.get('DB_PASSWORD')
+HOST = os.environ.get('DB_HOST')
+DB_NAME = os.environ.get('DB_NAME')
+
 # Поменять здесь при настройке Postgres в Docker
-SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@localhost/mydatabase"
+SQLALCHEMY_DATABASE_URL = f'postgresql://{USER}:{PASSWORD}@{HOST}/{DB_NAME}'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
