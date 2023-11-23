3b4b4cc88e018a8be0f10d5d443772f267fd5d93
Damir Nabiullin <nabiull2020@gmail.com>
2023-11-23 19:28:15 +0300

    Removed dal

--- quranapp_backend/src/DAL/enums.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class RiwayahEnum(Enum):
-    HAFS = "Hafs"
-    QALOON = "Qaloon"
--- quranapp_backend/src/dal/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.dal.models.user import User
-from src.dal.models.ayah_part import AyahPart
-from src.dal.models.recording import Recording
-from src.dal.models.ayah import Ayah
