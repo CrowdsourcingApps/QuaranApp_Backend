c37b85a3df10ef78f28890fc1537ab8190476470
Damir Nabiullin <nabiull2020@gmail.com>
2024-01-13 17:12:51 +0300

    Removed useles command

--- quranapp_backend/main.py
+++ quranapp_backend/main.py
@@ -1,11 +1,8 @@
-import sys
-import os
 import src.startup as startup
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from src.controllers import settings, users, recordings
 
-print(os.getcwd())
 
 app = FastAPI()
 
