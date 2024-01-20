947cb7edc4bf0637d4bb16de554f0686f388ce3d
Damir Nabiullin <nabiull2020@gmail.com>
2024-01-20 18:37:08 +0300

    Update tokens.py

--- quranapp_backend/src/controllers/tokens.py
+++ quranapp_backend/src/controllers/tokens.py
@@ -27,10 +27,11 @@ def generate_jwt_token(
     return TokensResponse(token=access_token)
 
 
-@token_router.get("/refresh")
+@token_router.get("/refresh", response_model=TokensResponse)
 def get_cookie(request: Request, db: Session = db_session_dependency):
     refresh_token = request.cookies.get("refresh_token")
     if not refresh_token:
         raise HTTPException(status_code=401, detail='Invalid user data')
 
-    return tokens_service.refresh_access_token(db=db, token=refresh_token)
+    access_token = tokens_service.refresh_access_token(db=db, token=refresh_token)
+    return TokensResponse(token=access_token)
