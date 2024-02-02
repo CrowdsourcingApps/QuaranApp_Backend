ab9b9e0a5dbece47d73d94c5fb5b276242679709
Damir Nabiullin <nabiull2020@gmail.com>
2024-02-02 20:46:00 +0300

    Added find user by prefix

--- quranapp_backend/tests/services/test_user_service.py
+++ quranapp_backend/tests/services/test_user_service.py
@@ -5,7 +5,7 @@ from src.services.users import check_if_alias_exists, get_user_by_id, get_user_b
 
 
 def test_create_user(db_session):
-    user_data = {'id': str(uuid.uuid4()), 'alias': 'test_alias', 'name': 'test_name', 'surname': 'test_surname'}
+    user_data = {'id': str(uuid.uuid4()), 'alias': ' tEsT_aLiAs ', 'name': '   Test_Name', 'surname': 'Test_Surname   '}
     user = User(**user_data)
 
     # Ensure user does not exist
@@ -18,7 +18,12 @@ def test_create_user(db_session):
     create_user(db_session, user)
 
     # Check if user now exists
+    user = get_user_by_id(db_session, user.id)
+
+    assert user is not None
+    assert user.alias == 'test_alias'
+    assert user.name == 'Test_Name'
+    assert user.surname == 'Test_Surname'
     assert check_if_alias_exists(db_session, user.alias)
-    assert get_user_by_id(db_session, user.id) is not None
     assert get_user_by_alias(db_session, user.alias) is not None
     assert delete_user(db_session, user.id)
