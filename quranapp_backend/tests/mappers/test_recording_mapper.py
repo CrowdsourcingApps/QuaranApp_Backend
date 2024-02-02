be8337c62437d19a0bcfd924342fd9621a61e2e7
Damir Nabiullin <nabiull2020@gmail.com>
2024-02-02 21:33:36 +0300

    Added service and mappers tests

---
+++ quranapp_backend/tests/mappers/test_recording_mapper.py
@@ -0,0 +1,16 @@
+from src.dal.enums import RiwayahEnum
+from src.mappers.recording_mapper import map_create_request_to_model
+from src.models import AyahPartSearch, RecordingCreate
+
+
+def test_map_create_request_to_model():
+    recording_create = map_create_request_to_model(
+        1, 1, 0,
+        2, 2, 1,
+        RiwayahEnum.QALOON)
+
+    assert recording_create is not None
+    assert type(recording_create) is RecordingCreate
+    assert recording_create.riwayah == RiwayahEnum.QALOON
+    assert recording_create.start == (AyahPartSearch(surah_number=1, ayah_in_surah_number=1, part_number=0))
+    assert recording_create.end == (AyahPartSearch(surah_number=2, ayah_in_surah_number=2, part_number=1))
