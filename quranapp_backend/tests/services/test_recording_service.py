65576034d920b2436f8145e8bc0c44fa2eac1e2a
Andrey Sobolev <sobolev.aii@yandex.ru>
2024-02-06 21:21:02 +0300

    Refactoring for recording service tests: replaced global variables with class attributes

--- quranapp_backend/tests/services/test_recording_service.py
+++ quranapp_backend/tests/services/test_recording_service.py
@@ -5,99 +5,83 @@ from src.services import ayah_parts
 from src.services.recordings import create_recording, get_recording_by_id, delete_recording, share_recording, \
     get_shared_with_me_recordings, get_my_recordings, get_available_recordings
 
-saved_recording: Recording
-start_ayah_part: AyahPart | None = None
-end_ayah_part: AyahPart | None = None
-audio_url: str = "https://example.com/audio.mp3"
 
+class TestRecordingService:
+    saved_recording: Recording | None = None
+    audio_url: str = "https://example.com/audio.mp3"
+    start_ayah_part: AyahPart | None = None
+    end_ayah_part: AyahPart | None = None
 
-def set_init_data(db_session):
-    global start_ayah_part, end_ayah_part
+    @classmethod
+    def set_init_data(cls, db_session):
+        if cls.start_ayah_part is not None and cls.end_ayah_part is not None:
+            return
 
-    if start_ayah_part is not None and end_ayah_part is not None:
-        return
+        cls.start_ayah_part = ayah_parts.get_ayah_part(
+            db_session, AyahPartSearch(surah_number=1, ayah_in_surah_number=0, part_number=0),
+            RiwayahEnum.QALOON, PublisherEnum.MADINA
+        )
+        cls.end_ayah_part = ayah_parts.get_ayah_part(
+            db_session, AyahPartSearch(surah_number=1, ayah_in_surah_number=1, part_number=0),
+            RiwayahEnum.QALOON, PublisherEnum.MADINA
+        )
 
-    start_ayah_part = ayah_parts.get_ayah_part(
-        db_session, AyahPartSearch(surah_number=1, ayah_in_surah_number=0, part_number=0),
-        RiwayahEnum.QALOON, PublisherEnum.MADINA
-    )
-    end_ayah_part = ayah_parts.get_ayah_part(
-        db_session, AyahPartSearch(surah_number=1, ayah_in_surah_number=1, part_number=0),
-        RiwayahEnum.QALOON, PublisherEnum.MADINA
-    )
+    def test_create_recording(self, db_session, user1):
+        test_class = type(self)
 
+        user_id = user1.id
+        self.set_init_data(db_session)
 
-def test_create_recording(db_session, user1):
-    global saved_recording
+        recording = create_recording(db_session, user_id, self.start_ayah_part, self.end_ayah_part, self.audio_url)
+        # сохраняем рекординг в атрибут класса, а не в атрибут инстанса
+        test_class.saved_recording = recording
 
-    user_id = user1.id
-    set_init_data(db_session)
+        assert recording.user_id == user_id
+        assert recording.start == self.start_ayah_part
+        assert recording.end == self.end_ayah_part
+        assert recording.audio_url == self.audio_url
 
-    recording = create_recording(db_session, user_id, start_ayah_part, end_ayah_part, audio_url)
-    saved_recording = recording
+    def test_get_recording_by_id(self, db_session):
+        recording = get_recording_by_id(db_session, self.saved_recording.id)
 
-    assert recording.user_id == user_id
-    assert recording.start == start_ayah_part
-    assert recording.end == end_ayah_part
-    assert recording.audio_url == audio_url
+        assert recording is not None
+        assert recording.id == self.saved_recording.id
+        assert recording.audio_url == self.saved_recording.audio_url
+        assert recording.user_id == self.saved_recording.user_id
 
+    def test_share_recording(self, db_session, user2):
+        recording = share_recording(db_session, self.saved_recording.id, user2.id)
 
-def test_get_recording_by_id(db_session):
-    global saved_recording
+        assert recording is not None
+        assert recording.recipient_id == user2.id
+        assert len(get_shared_with_me_recordings(db_session, user2.id)) == 1
 
-    recording = get_recording_by_id(db_session, saved_recording.id)
+    def test_get_shared_with_me_recordings(self, db_session, user1, user2):
+        assert len(get_shared_with_me_recordings(db_session, user1.id)) == 0
+        assert len(get_shared_with_me_recordings(db_session, user2.id)) == 1
 
-    assert recording is not None
-    assert recording.id == saved_recording.id
-    assert recording.audio_url == saved_recording.audio_url
-    assert recording.user_id == saved_recording.user_id
+    def test_get_my_recordings(self, db_session, user1):
+        recordings = get_my_recordings(db_session, user1.id)
 
+        assert len(recordings) == 1
+        assert recordings[0].id == self.saved_recording.id
 
-def test_share_recording(db_session, user2):
-    global saved_recording
+    def test_get_available_recordings(self, db_session, user1, user2):
+        user1_recordings = get_available_recordings(db_session, user1.id)
+        user2_recordings = get_available_recordings(db_session, user1.id)
 
-    recording = share_recording(db_session, saved_recording.id, user2.id)
+        assert len(user1_recordings) == 1
+        assert len(user2_recordings) == 1
+        assert user1_recordings[0].id == self.saved_recording.id
+        assert user2_recordings[0].id == self.saved_recording.id
 
-    assert recording is not None
-    assert recording.recipient_id == user2.id
-    assert len(get_shared_with_me_recordings(db_session, user2.id)) == 1
+    def test_delete_recording(self, db_session, user2):
+        delete_recording(db_session, self.saved_recording.id)
+        recording = db_session.get(Recording, self.saved_recording.id)
 
+        assert recording.is_deleted
+        assert len(get_shared_with_me_recordings(db_session, user2.id)) == 0
 
-def test_get_shared_with_me_recordings(db_session, user1, user2):
-    assert len(get_shared_with_me_recordings(db_session, user1.id)) == 0
-    assert len(get_shared_with_me_recordings(db_session, user2.id)) == 1
-
-
-def test_get_my_recordings(db_session, user1):
-    global saved_recording
-
-    recordings = get_my_recordings(db_session, user1.id)
-
-    assert len(recordings) == 1
-    assert recordings[0].id == saved_recording.id
-
-
-def test_get_available_recordings(db_session, user1, user2):
-    global saved_recording
-
-    user1_recordings = get_available_recordings(db_session, user1.id)
-    user2_recordings = get_available_recordings(db_session, user1.id)
-
-    assert len(user1_recordings) == 1
-    assert len(user2_recordings) == 1
-    assert user1_recordings[0].id == saved_recording.id
-    assert user2_recordings[0].id == saved_recording.id
-
-
-def test_delete_recording(db_session, user2):
-    global saved_recording
-
-    delete_recording(db_session, saved_recording.id)
-    recording = db_session.get(Recording, saved_recording.id)
-
-    assert recording.is_deleted
-    assert len(get_shared_with_me_recordings(db_session, user2.id)) == 0
-
-    # Clear db
-    db_session.delete(recording)
-    db_session.commit()
+        # Clear db
+        db_session.delete(recording)
+        db_session.commit()
