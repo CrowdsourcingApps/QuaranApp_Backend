6b1f9523b173bed491fb2bb23b7f17e7feb84f89
Andrey Sobolev <sobolev.aii@yandex.ru>
2024-02-08 18:22:49 +0300

    Fixed todo comment

--- quranapp_backend/src/dal/utils.py
+++ quranapp_backend/src/dal/utils.py
@@ -4,16 +4,15 @@ from src.dal.models import Mushaf, Ayah
 from src.dal.enums import RiwayahEnum, PublisherEnum
 
 
-# todo после того, как переведем БД на модель v2 на heroku инстансе, скрипты ниже можно удалить
-
-#===========================
-
 def create_default_mushaf(session: Session) -> Mushaf:
     db_mushaf = Mushaf(publisher=PublisherEnum.MADINA, riwayah=RiwayahEnum.QALOON)
     session.add(db_mushaf)
     session.commit()
     session.refresh(db_mushaf)
     return db_mushaf
+
+# todo после того, как переведем БД на модель v2 на heroku инстансе, скрипт ниже можно удалить
+#===========================
 
 
 def fill_mushaf_for_existing_ayahs(session: Session):
