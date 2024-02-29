5f20a9088deecc4bb45843103782aa63db6ee138
Damir Nabiullin <nabiull2020@gmail.com>
2024-02-29 22:30:14 +0300

    Added simple ayah_parts and markers load

--- quranapp_backend/src/services/mushaf_pages.py
+++ quranapp_backend/src/services/mushaf_pages.py
@@ -10,8 +10,8 @@ def get_pages_by_mushaf_id(db: Session, mushaf_id: uuid.UUID) -> list[MushafPage
 
 
 def get_ayah_parts_and_markers_by_page_id(db: Session, page_id: uuid.UUID):
-    return db.query(AyahPartMarker) \
-            .join(AyahPart, AyahPart.id == AyahPartMarker.ayah_part_id) \
+    return db.query(AyahPart) \
+            .join(AyahPartMarker, AyahPart.id == AyahPartMarker.ayah_part_id) \
             .join(Ayah, Ayah.id == AyahPart.ayah_id) \
             .filter(AyahPart.mushaf_page_id == page_id) \
             .order_by(Ayah.surah_number, Ayah.ayah_in_surah_number) \
