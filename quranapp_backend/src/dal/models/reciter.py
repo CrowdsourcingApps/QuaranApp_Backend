3b274e070975b79ba3f6a56ae198b154f55b8987
Damir Nabiullin <nabiull2020@gmail.com>
2024-03-24 14:58:46 +0300

    Added reciter audios

--- quranapp_backend/src/dal/models/reciter.py
+++ quranapp_backend/src/dal/models/reciter.py
@@ -2,6 +2,7 @@ import uuid
 
 from sqlalchemy import Column, String, Enum
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 from src.dal.database import Base
 from src.dal.enums import RiwayahEnum
@@ -12,3 +13,5 @@ class Reciter(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
     riwayah = Column(Enum(RiwayahEnum), nullable=False)
+
+    audios = relationship("ReciterAudio", back_populates="reciter")
--- quranapp_backend/src/mappers/ayah_part_mapper.py
+++ quranapp_backend/src/mappers/ayah_part_mapper.py
@@ -1,6 +1,6 @@
 import uuid
 
-from src.dal.models import AyahPart as AyahPartDal
+from src.dal.models import AyahPart as AyahPartDal, AyahPartText
 from src.dal.models import AyahPartMarker as AyahPartMarkerDal
 from src.models import MushafPageDetails, MushafPageAyahPart, AyahPartMarker
 
@@ -13,8 +13,9 @@ def map_to_page_details(page_id: uuid.UUID, ayah_parts: list[AyahPartDal]) -> Mu
             part_number=ayah_part.part_number,
             surah_number=ayah_part.ayah.surah_number,
             ayah_in_surah_number=ayah_part.ayah.ayah_in_surah_number,
-            text=ayah_part.text.text,
+            text=_map_text(ayah_part.text),
             text_id=ayah_part.ayah_part_text_id,
+            audio_link=_map_reciter_audio(ayah_part.text),
             markers=_map_ayah_part_markers(ayah_part.markers)
         ))
 
@@ -29,6 +30,7 @@ def _map_ayah_part_markers(markers: list[AyahPartMarkerDal]):
     mapped_markers = []
     for marker in markers:
         mapped_markers.append(AyahPartMarker(
+            id=marker.id,
             order=marker.order,
             x=marker.x,
             y1=marker.y1,
@@ -37,3 +39,18 @@ def _map_ayah_part_markers(markers: list[AyahPartMarkerDal]):
         ))
 
     return sorted(mapped_markers, key=lambda m: m.order)
+
+
+def _map_text(text: AyahPartText) -> str | None:
+    if text is None:
+        return None
+
+    return text.text
+
+
+def _map_reciter_audio(text: AyahPartText) -> str | None:
+    if text is None:
+        return None
+
+    audios = text.reciter_audios
+    return audios[0].audio_link
