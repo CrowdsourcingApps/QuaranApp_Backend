50e9dc13e6ebea63f5fcebe8585b05d920455d3e
Andrey Sobolev <sobolev.aii@yandex.ru>
2024-06-16 12:33:32 +0300

    Added statistics collection for a dataset, changed the way how not recognized audio chunks are handled

--- quranapp_backend/dataset/utils.py
+++ quranapp_backend/dataset/utils.py
@@ -19,3 +19,32 @@ def find_two_largest_values_and_indexes(input_values_list: list[float]):
             second_largest_index = i
 
     return largest_index, second_largest_index, largest, second_largest
+
+
+def check_if_chunk_considered_incorrect(errors_by_reviewer_info: list[tuple]) -> bool:
+    reviewers_considered_incorrect = 0
+    for errors_by_reviewer in errors_by_reviewer_info:
+        # errors dict is not empty
+        if errors_by_reviewer[1]:
+            reviewers_considered_incorrect += 1
+
+    return reviewers_considered_incorrect / len(errors_by_reviewer_info) >= 0.5
+
+
+def get_formatted_chunks_by_surah_count(
+        surah_number_to_title_mapping: dict, chunks_by_surah_count: dict[str, dict]
+) -> dict:
+    chunks_by_surah_count = {
+        surah_number_to_title_mapping[k]: {"count_by_riwayah": dict(v), "surah_number": k}
+        for k, v in chunks_by_surah_count.items()
+    }
+    sorted_chunks_by_surah_counter = {
+        k: chunks_by_surah_count[k]
+        for k in sorted(
+            chunks_by_surah_count,
+            key=lambda x: sum(chunks_by_surah_count[x]["count_by_riwayah"].values()),
+            reverse=True
+        )
+    }
+    return sorted_chunks_by_surah_counter
+
